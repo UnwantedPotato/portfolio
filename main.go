dab1ac5894276e6349fd4c6a252ecf1f4d9c25df
Jonathan Austin <johnybobaustin@gmail.com>
2026-07-19 22:03:28 -0500

    decided on color palette (probably)

--- main.go
+++ main.go
@@ -1,19 +1,13 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 )
-
-func clicked(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<p style='color: green;'>Content swapped dynamically by htmx!</p>")
-}
 
 func main() {
 	mux := http.NewServeMux()
 
 	fileServer := http.FileServer(http.Dir("./static"))
 	mux.Handle("GET /", fileServer)
-	mux.HandleFunc("GET /clicked", clicked)
 	http.ListenAndServe(":8080", mux)
 }
